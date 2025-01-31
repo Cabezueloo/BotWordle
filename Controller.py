df69fa4084538bc02804117f4b762915257d8cb3
cabezueloo <angelcabezuelovert@gmail.com>
2025-01-31 17:12:03 +0100

    first commit

---
+++ Controller.py
@@ -0,0 +1,117 @@
+'''
+'''
+import time
+
+BAD = 'b'
+GOOD = 'g'
+REGULAR = 'r'
+NO_EXIST = "."
+
+class Controller:
+    
+    def createList(self) -> list:
+        
+        
+        f = open('dictionary_spanish.txt', 'r')
+
+        listaWords = f.read().split("\n")
+
+        f.close()
+        return listaWords
+    
+    def __init__(self):
+        self.listWords = self.createList()
+        
+           
+    def loop(self):
+        
+        wordAttemp = input("Word -> ").lower()
+        blockCode : str = input("G/R/B -> ").lower()
+        
+        while True:
+
+            t = time.time()
+
+            listLetterInBadPosition = []
+            
+            letterInGoodPosition = dict()
+            listLetterInGoodPosition = []
+            letterInRegularPosition = dict()
+            
+            regularLetterList = []
+            
+            for index, char in enumerate(wordAttemp):
+                if ( blockCode[index].__eq__(BAD)):
+                    listLetterInBadPosition.append(char)
+                elif ( blockCode[index].__eq__(GOOD)):
+                    letterInGoodPosition[index] = char
+                    listLetterInGoodPosition.append(char)
+                elif ( blockCode[index].__eq__(REGULAR)):
+                    letterInRegularPosition[index] = char
+                    regularLetterList.append(char)
+                
+            lenOriginal = len(self.listWords)
+            originalList = self.listWords.copy()
+
+
+            #For in all the words dicitonaray
+            for index, word in enumerate(originalList):
+                deleted = False
+                
+
+                for indexLetter, letter in enumerate(word):
+
+                    #GOOD
+                    if ( letterInGoodPosition.__contains__(indexLetter)):
+                        if (letterInGoodPosition.get(indexLetter) != letter):
+                            deleted= True
+                            break
+                
+                    #Regular
+                    if ( letterInRegularPosition.__contains__(indexLetter)):
+                        if(letterInRegularPosition.get(indexLetter) == letter):
+                            deleted = True
+                            break
+                    
+                    
+                    if (letter in listLetterInBadPosition and not letter in regularLetterList and not letter in listLetterInGoodPosition) :
+                        deleted = True
+                    
+                for rword in regularLetterList:
+                    if not rword in word:
+                        deleted = True
+                        break
+
+
+                
+                if deleted:
+                    self.listWords.remove(word)
+
+            print(f"Tiempo en recorrer y comprobar todo el diccionario -> {time.time() - t }")
+            print(f"Tamagno antes de iterar -> {lenOriginal}" )
+            print(f"Tamagno despues de iterar -> {len(self.listWords)}" )
+            print(f"Existe todos -> {(self.listWords.__contains__("todos"))}" )
+            #check : str= input("Check:")
+
+            #if (check != ""):
+             #   print(self.listWords.__contains__(check))
+            
+            if(self.listWords.__contains__(wordAttemp)):
+                self.listWords.remove(wordAttemp)
+            
+            wordAttemp = self.listWords[0]
+
+
+            print(f"Try -> {wordAttemp}")
+
+
+            blockCode : str = input("G/R/B -> ").lower()
+        
+            
+
+
+controller = Controller()
+
+controller.loop()
+
+    
